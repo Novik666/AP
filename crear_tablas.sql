88c0554fe89ee7e6e929ac9518d3eeb52cabb2e7
Novik666 <francis_francis666@hotmail.com>
2023-07-13 03:38:18 -0400

    se agrega el proyecto

---
+++ crear_tablas.sql
@@ -0,0 +1,12 @@
+create table productos(
+codigo_p varchar(4),
+marca varchar(30),
+modelo varchar(30),
+color varchar(30),
+precio float,
+proveedor varchar(30));
+
+insert into productos values
+('11AR','Bike','XTKM','Rojo',2501.2,'Homets'),
+('12TG','Specialized','Specialized Tarmac','Negro',2222.2,'Ajams'),
+('T1AU','Cannondale','Cannondale Scalpel','Verde',5000.2,'Lanscorp');
